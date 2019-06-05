cbf623541162e6956a207c8e8776d316e204726e
Guilherme Guy <guilhermeguy349@gmail.com>
2019-06-05 10:53:04 -0300

    Updated list view

--- TestesTBL/TransactionalFunctions/views.py
+++ TestesTBL/TransactionalFunctions/views.py
@@ -18,6 +18,26 @@ class TransactionList(ListView):
     def get_queryset(self):
         return self.model.objects.all()
 
+    def get_by_transactions_type(self):
+        return {
+            'EE': self.sum_points_of_class(1),
+            'CE': self.sum_points_of_class(2),
+            'SE': self.sum_points_of_class(3)
+        }
+
+    def sum_points_of_class(self, type):
+
+        transactions = TransactionalFunction.objects.filter(functionality_type=type)
+
+        return sum(x.function_points_aumount for x in transactions)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['transactions_by_type'] = self.get_by_transactions_type()
+
+        return context
+
 
 class TransactionCreate(CreateView):
 
